a3c35d70122d5a3bd5b7aeb392273707fb74c879
Jeyoon <rj1031@naver.com>
2023-01-13 13:37:00 +0900

    day02 datatype tuple

--- day02.py
+++ day02.py
@@ -1,2 +1,15 @@
-PI = 3.14
-print(f'원주율의 값은 {PI}이고 타입은 {type(PI)}입니다')
+# Chap 3
+
+print(2*2*2*2*2)
+print(2**5)
+print(pow(2, 5))
+print(divmod(9, 5))
+print(type(divmod(9, 5)))
+print(type((1,2)))
+test = 1, 2 #packing
+print(type(test))
+print(test)
+print((test[1]))
+a, b = test # unpacking
+print(a)
+print(b)
