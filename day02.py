60d71b0eb5bf7bbb8bc0d8277bc51bac41cf106d
Jeyoon <rj1031@naver.com>
2023-01-13 15:22:18 +0900

    day02 if

--- day02.py
+++ day02.py
@@ -1,14 +1,26 @@
-# Chap 3
+# Chap 4 if
 
-# number = 0b10011010
-# number = 0x9A
-# number = 0o232
-# print(number)
+limits = 20
+tweets = "pass" * 6
+diff = limits - len(tweets)
+# if diff := limits - len(tweets) >= 0:
+if diff >=0:
+    print(tweets)
+else:
+    print(f'제한 글자 수 {abs(diff)}초과')
 
-number = 154
-print(bin(number))
-print(hex(number))
-print(oct(number))
+vowels = 'aeiou'
+letter = 'u'
+if letter not in vowels:
+    print("실행 안됨!")
+else:
+    print("실행 됨!")
 
-print(ord("a")) #ascii
-print(hex(ord(" "))) #20
+a = []
+
+print(bool(a))
+a.append(5)
+print(bool(a))
+print(bool(set()))
+print(bool(dict()))
+print(bool("ace"))
