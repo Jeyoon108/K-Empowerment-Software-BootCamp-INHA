a71b8255562088bd6894b0b6c11c7dcc20a2d619
Jeyoon <rj1031@naver.com>
2023-01-16 14:09:12 +0900

    day03_03

--- day03.py
+++ day03.py
@@ -1,17 +1,40 @@
-# practice #1 - m으로 시작하는 단어를 대문자로 만들어보자.
-song_1 = """When an eel grabs your arm,
-And it causes great harm,
-That's - a moray!"""
+# while문 사용하여 구구단 만들기
+#
+# dan = int(input('Dan : '))
+# i = 1
+#
+# while i < 10:
+#     #print(f'{dan} * {i} = {dan * i}')
+#     print('{0} * {1} = {2}'.format(dan, i, dan * i))
+#     i += 1
 
-print(song_1.replace(' m',' M'))
+# 1~9단만 사용할 수 있게, 틀릴시 다시 진행
 
-# or
+# while True:
+#     dan = int(input('Dan : '))
+#
+#     if 1 < dan < 10:
+#         i=1
+#         while i < 10:
+#             print('{0} * {1} = {2}'.format(dan, i, dan * i))
+#             i += 1
+#         break
+#     else:
+#             print('2에서 9사이의 값을 입력하세요')
 
-song_2 = """When an eel grabs your arm,
-And it causes great harm,
-That's - a moray!"""
+# while True:
+#     stuff = input("String to capitalize [type q to quit]: ")
+#     if stuff == "q":
+#         break
+#     print(stuff.capitalize())
 
-song_list = song_2.split()
-song_list[14] = song_list[14].title()
-song_string = ' '.join(song_list)
-print(song_string.replace(', ',',\n'))
+# numbers = [1, 10, 5]
+# position = 0
+# while position < len(numbers):
+#     number =numbers[position]
+#     if number % 2 == 0:
+#         print('Found even number', number)
+#         break
+#     position += 1
+# else:
+#     print('No even number found')
