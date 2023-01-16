e1e09081fe16370934e875e2bd97a216021ca673
Jeyoon <rj1031@naver.com>
2023-01-16 14:43:02 +0900

    day03_04

--- day03.py
+++ day03.py
@@ -1,40 +1,56 @@
-# while문 사용하여 구구단 만들기
-#
-# dan = int(input('Dan : '))
-# i = 1
-#
-# while i < 10:
-#     #print(f'{dan} * {i} = {dan * i}')
-#     print('{0} * {1} = {2}'.format(dan, i, dan * i))
+# prime number
+
+number = int(input("input number : "))
+counts = 0
+
+i = 1
+
+# while i <= number:
+#     if number % i == 0:
+#         counts += 1
 #     i += 1
-
-# 1~9단만 사용할 수 있게, 틀릴시 다시 진행
-
-# while True:
-#     dan = int(input('Dan : '))
-#
-#     if 1 < dan < 10:
-#         i=1
-#         while i < 10:
-#             print('{0} * {1} = {2}'.format(dan, i, dan * i))
-#             i += 1
-#         break
-#     else:
-#             print('2에서 9사이의 값을 입력하세요')
-
-# while True:
-#     stuff = input("String to capitalize [type q to quit]: ")
-#     if stuff == "q":
-#         break
-#     print(stuff.capitalize())
-
-# numbers = [1, 10, 5]
-# position = 0
-# while position < len(numbers):
-#     number =numbers[position]
-#     if number % 2 == 0:
-#         print('Found even number', number)
-#         break
-#     position += 1
+# if counts == 2:
+#     print(f'{number} is a prime number!')
 # else:
-#     print('No even number found')
+#     print(f'{number} is not a prime number!')
+
+
+# for i in range(1,number + 1):
+#     if number % i == 0:
+#         counts += 1
+# if counts == 2:
+#     print(f'{number} is a prime number!')
+# else:
+#     print(f'{number} is not a prime number!')
+
+
+
+# for i in range(2, number):
+#     if number % i == 0:
+#         counts += 1
+# if counts:
+#     print(f'{number} is not a prime number!')
+# else:
+#     print(f'{number} is a prime number!')
+
+
+# for i in range(2, number):
+#     if number % i == 0:
+#         counts += 1
+# if counts:
+#     print(f'{number} is not a prime number!')
+# else:
+#     print(f'{number} is a prime number!')
+
+is_prime = True
+
+for i in range(2, number):
+    if number % i == 0:
+        is_prime = False
+        break
+    print(i)
+
+if is_prime:
+    print(f'{number} is a prime number!')
+else:
+    print(f'{number} is not a prime number!')
