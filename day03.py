577e5ccf3702d71cd83909e7a438056296c9d3d8
Jeyoon <rj1031@naver.com>
2023-01-16 15:35:17 +0900

    day03_06

--- day03.py
+++ day03.py
@@ -1,13 +1,16 @@
 # input 2 numbers
 
-# start = int(input("start number : "))
-# end = int(input("end number : "))
+start = int(input("input start number : "))
+end = int(input("input end number : "))
 
-# print(start, end)
-#
-# for i in range(start, end+1):
-#     print(i, end=' ')
+if end < start:
+    start, end = end, start
 
-# start_end = input("start and end number : ").split()
-# print(start_end)
-# print(int(start_end[0]), int(start_end[1]))
+for i in range(start, end + 1):
+    if i <= 1:
+        continue
+    for k in range(2, i):
+        if i % k == 0:
+            break
+    else:
+        print(i, end = ' ')
