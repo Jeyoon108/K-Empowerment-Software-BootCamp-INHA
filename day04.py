7637738746d1d2376947e76faa9a8ee34eb926bc
Jeyoon <rj1031@naver.com>
2023-01-17 15:02:38 +0900

    day04_06

--- day04.py
+++ day04.py
@@ -1,8 +1,41 @@
-# Chap 7 #4 ~ #7
+# dictionary
 
-things = ['mozzarella', 'cinderella', 'salmonella']
-things[1] = things[1].capitalize()
-things[0] = things[0].upper()
-print(things)
-print(f'Delete the \'{things.pop()}\' from things, get Nobel Prize')
-print(things)
+# students = {'name': 'kim inha', 'age': 23, 'eyes': [0.9, 1.1]}
+# # for k in students.keys():
+# # for k in students:
+# # for k in students.values():
+# for k, v in students.items():
+#     # print(k)
+#     print(f'{k} : {v}')
+# print(f"이름은 {students['name']}, 나이는 {students['age']}세", end=' ')
+# print(f'시력은 좌:{students["eyes"][0]}, 우:{students["eyes"][1]}')
+
+# alcohol dictionary
+
+import random
+
+alcohol_foods = {
+    '맥주': '치킨',
+    '소주': '골뱅이소면',
+    '와인': '치즈',
+    '고량주': '짬뽕'
+}
+alcohol_list = list(alcohol_foods)  # key값들만 추출
+while True:
+    alcohol = input(f'술을 선택 하세요. 1) {alcohol_list[0]} 2) {alcohol_list[1]} 3) {alcohol_list[2]} 4) {alcohol_list[3]} 5) 아무거나 6) 계산 '
+                    f'\n메뉴 입력 : ')
+    if alcohol == '6':
+        print('계산 완료!')
+        break
+    elif alcohol == '1':
+        print(f'{alcohol_list[0]}에 어울리는 안주는 {alcohol_foods[alcohol_list[0]]}입니다')
+    elif alcohol == '2':
+        print(f'{alcohol_list[1]}에 어울리는 안주는 {alcohol_foods[alcohol_list[1]]}입니다')
+    elif alcohol == '3':
+        print(f'{alcohol_list[2]}에 어울리는 안주는 {alcohol_foods[alcohol_list[2]]}입니다')
+    elif alcohol == '4':
+        print(f'{alcohol_list[3]}에 어울리는 안주는 {alcohol_foods[alcohol_list[3]]}입니다')
+    elif alcohol == '5':
+        print(f'{alcohol_list[random.randint(0,3)]}에 {alcohol_foods[alcohol_list[random.randint(0,3)]]}을(를) 드리겠습니다')
+    else:
+        print('해당하는 번호의 메뉴가 없습니다')
