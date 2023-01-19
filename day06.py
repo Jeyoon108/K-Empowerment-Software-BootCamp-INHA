cf4cd54f27dbc1a816327b8817200adfe4555452
Jeyoon <rj1031@naver.com>
2023-01-19 14:05:10 +0900

    day06_09_exception

--- day06.py
+++ day06.py
@@ -36,7 +36,8 @@
 # 사용자 정의 예외
 
 try:
-    raise Exception("쉬는 시간")
+    # raise Exception("쉬는 시간")
+    raise TypeError("타입 에러")
     expr = input('분자 분모 입력 (띄어쓰기로 구분): ').split()
     print(int(expr[0]) / int(expr[1]))
 # ValueError
