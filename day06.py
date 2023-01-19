13eddd11fdbf01931632a1ea0c49251451dd703f
Jeyoon <rj1031@naver.com>
2023-01-19 11:08:55 +0900

    day06_05_local,global variable

--- day06.py
+++ day06.py
@@ -1,35 +1,10 @@
-# generator 예제
+# local, global variable
 
-# decorator
-def document_it(func):
-    def new_function(*args, **kwargs):
-        print('Running function:', func.__name__)  # 실행중인 함수
-        print('Positional arguments:', args)  # 위치 기반 인수들
-        print('Keyword arguments:', kwargs)  # 키워드 기반 인수들
-        result = func(*args, **kwargs)
-        print('Result:', result)  # 실행 결과
-        return result
-    return new_function
+#g = 1
+def print_global():
+    g = 1
+    print(g)
 
 
-def square_it(func):
-    def new_function(*args, **kwargs):
-        result = func(*args, **kwargs)
-        return result * result
-    return new_function
-
-
-@document_it
-@square_it
-def add_ints(a, b):
-    return a + b
-
-@document_it
-def squares(n):
-    return n * n
-
-
-# # @decorator_name은 def에 직접 사용
-
-add_ints(3, 5)
-print(squares(5))
+print(g)
+print_global()
