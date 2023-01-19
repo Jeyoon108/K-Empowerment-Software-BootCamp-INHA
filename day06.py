4d4e4684340f31700f01342e267996a1c2785674
Jeyoon <rj1031@naver.com>
2023-01-19 10:45:58 +0900

    day06_04_decorator

--- day06.py
+++ day06.py
@@ -2,7 +2,7 @@
 
 # decorator
 def document_it(func):
-    def new_function(*args,**kwargs):
+    def new_function(*args, **kwargs):
         print('Running function:', func.__name__)  # 실행중인 함수
         print('Positional arguments:', args)  # 위치 기반 인수들
         print('Keyword arguments:', kwargs)  # 키워드 기반 인수들
@@ -12,11 +12,24 @@ def document_it(func):
     return new_function
 
 
+def square_it(func):
+    def new_function(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return result * result
+    return new_function
+
+
 @document_it
+@square_it
 def add_ints(a, b):
     return a + b
+
+@document_it
+def squares(n):
+    return n * n
 
 
 # # @decorator_name은 def에 직접 사용
 
 add_ints(3, 5)
+print(squares(5))
