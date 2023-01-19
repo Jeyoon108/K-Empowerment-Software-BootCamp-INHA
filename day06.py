ca3b63d1dbd48d5666ca5ed1bdcb92fe1d25cd12
Jeyoon <rj1031@naver.com>
2023-01-19 11:21:58 +0900

    day06_06_local,global variable

--- day06.py
+++ day06.py
@@ -1,10 +1,26 @@
 # local, global variable
 
-#g = 1
+g = 1
 def print_global():
-    g = 1
+    g = 3
     print(g)
 
 
-print(g)
+# def change_print_global():
+#     print(g)
+#     g = 2
+#     print(g)  >> error code
+
+
+def change_print_global():
+    global g
+    print(g)
+    g = 2
+    print(g)
+
+
 print_global()
+change_print_global()
+print(g)
+print(globals())
+print(__name__)
