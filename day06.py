e9857c8cb78c0c2868ac86ede4e5dfa890698e65
Jeyoon <rj1031@naver.com>
2023-01-19 16:14:10 +0900

    day06_13_class - inheritance

--- day06.py
+++ day06.py
@@ -1,4 +1,4 @@
-# class
+# class - inheritance
 
 # class Pokemon:
 #     def __init__(self):  # 객체 생성 시 동작
@@ -23,13 +23,26 @@ class Pokemon:
             print(skill)
 
 
+
+class Pikachu(Pokemon):  # inheritance
+    pass
+
+# class Pikachu():  # 만일 inheritance 하지않으려면
+#     def __init__(self, name, owner, skills):  # 객체 생성 시 동작 # skill을 구현할 때 스킬이 하나가 아니니 자료구조를 준비한다 (만만한게 리스트)
+#         self.name = name
+#         self.owner = owner
+#         self.skills = skills.split('/')
+#         print(f"포켓몬 {name} 생성됨")
+#
+#
+#     def info(self):  # 멤버 함수
+#         print(f"{self.owner}의 포켓몬은 {self.name}입니다")
+#         for skill in self.skills:
+#             print(skill)
+
+pi1 = Pikachu('파이리', '웅이', '스핀 테일/브레스/몸통박치기')
+pi1.info()
+
 p1 = Pokemon('피카츄', '지우', '50만 볼트/100만 볼트/번개')
 p2 = Pokemon('꼬부기', '오박사', '고속 스핀/물대포/몸통박치기/할퀴기')
-# print(p1, p2)  # 서로 다른 메모리 주소를 가진다.
-print(p1.name)
-print(p2.name)
-# print(f"{p1.owner}의 포켓몬은 {p1.name}입니다")
-# print(f"{p2.owner}의 포켓몬은 {p2.name}입니다")
-p1.info()
-p2.info()
-# print(p2.skills)
+
