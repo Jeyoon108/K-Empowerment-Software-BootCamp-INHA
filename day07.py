d9313a50a7569eaf23fb63b4275373583e3cbfa3
Jeyoon <rj1031@naver.com>
2023-01-20 14:55:35 +0900

    day07_05_polymorphism

--- day07.py
+++ day07.py
@@ -1,25 +1,50 @@
-#
+# doc typing - polymorphism
+import math
 
-class PrettyMixin():
-    def time_print(self):
-        import datetime
-        print(datetime.date.today())
+class Shape:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+    def get_area(self):
+        print('도형의 면적을 출력합니다')
 
-    def dump(self):
-        import pprint
-        pprint.pprint(vars(self))
+class Circle(Shape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
+        self.radius = radius
 
+    def get_area(self):
+        return math.pi * self.radius * self.radius
 
-class Thing(PrettyMixin):
-    pass
+class Cylinder(Circle):
+    def __init__(self, x, y, radius, height):
+        super().__init__(x, y,radius)
+        self.height = height
 
+    def get_area(self):  # get_volume
+        # return math.pi * self.radius * self.radius * self.height # 이 공식 중 math.pi * self.radius * self.radius는 이미 있으므로 가져다 쓰면 된다.
+        return super().get_area() * self.height
 
-t = Thing()
-t.name = "Nyarlathotep"
-t.feature = "ichor"
-t.age = "eldritch"
-t.gender = "male"
-t.dump()
+class Rectangle(Shape):
+    def __init__(self, x, y, width, length):
+        super().__init__(x, y)
+        self.width = width
+        self.length = length
 
-t.time_print()
+    def get_area(self):
+        return self.width * self.length
+
+cy1 = Cylinder(20, 20, 10.0, 2)
+c1 = Circle(100, 100, 10.0)
+c2 = Circle(50, 50, 2.0)
+r1 = Rectangle(100, 50, 5, 2)
+
+print(r1.get_area())
+print(c1.get_area())
+print(c2.get_area())
+print(f'사각형의 좌표는 x:{r1.x}, y:{r1.y}이고 넓이는 {r1.get_area()}')
+print(cy1.get_area())
+
+# 만일 원기둥 만들시는 circle을 상속받는것이 좋다 >> 타고 올라가서 Shape도 상속 받을 수 있고 부피, 면적을 구할 때 circle의 면적을 사용할수도 있기 때문
+
