e7fa9fe62ad9e6b7f00633a290d7ddfe2bfba317
Jeyoon <rj1031@naver.com>
2023-01-23 22:50:52 +0900

    A2Z_Ch6_03

--- 2023_01_23.py
+++ 2023_01_23.py
@@ -1,21 +1,15 @@
 # A to Z reminding
 
-# Ch6 practice # 2
+# Ch6 practice # 3
 import random
+guess_me = random.randint(0, 9)
+print(guess_me)
 
-guess_me = random.randint(1, 10)
-number = random.randint(1, 10)
-# guess_me = 7
-# number = 1
-print(f'number : {number}, guess number : {guess_me}')
-
-while True:
-    if number == guess_me:
+for number in range(10):
+    if number < guess_me:
+        print(f'too low, {number}')
+    elif number == guess_me:
         print('found it!')
         break
-    elif number < guess_me:
-        print(f'too low, number is {number}')
-        number += 1
     else:
         print('oops')
-        break
