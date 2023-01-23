5536e6a0c44554dbe28624939565a5dc0630e3d3
Jeyoon <rj1031@naver.com>
2023-01-23 22:45:35 +0900

    A2Z_Ch6_02

--- 2023_01_23.py
+++ 2023_01_23.py
@@ -1,19 +1,21 @@
 # A to Z reminding
 
-# numbers = [2, 3, 5]
-# position = 0
-# while position < len(numbers):
-#     number = numbers[position]
-#     if number % 2 == 0:
-#         print('Found even number', number)
-#         break
-#     position += 1
-# else:
-#     print('No even number found')
+# Ch6 practice # 2
+import random
 
-# Ch6 practice # 1
+guess_me = random.randint(1, 10)
+number = random.randint(1, 10)
+# guess_me = 7
+# number = 1
+print(f'number : {number}, guess number : {guess_me}')
 
-numbers = list()
-for number in range(0, 4):
-    numbers.append(-number + 3)
-print(numbers)
+while True:
+    if number == guess_me:
+        print('found it!')
+        break
+    elif number < guess_me:
+        print(f'too low, number is {number}')
+        number += 1
+    else:
+        print('oops')
+        break
