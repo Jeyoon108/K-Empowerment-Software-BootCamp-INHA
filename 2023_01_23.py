da47530b91baf9f4ae64a00740afd1c0f71d9690
Jeyoon <rj1031@naver.com>
2023-01-23 19:44:48 +0900

    A2Z_Ch5_01

--- 2023_01_23.py
+++ 2023_01_23.py
@@ -1,18 +1,37 @@
 # A to Z reminding
 
-# Ch4 practice #2
-import random
+# Ch5 practice # 1
 
-small = random.choice([True, False])
-green = random.choice([True, False])
+song = """When an eel grabs your arm,
+And it causes great harm,
+That's - a moray!"""
 
-if small:
-    if green:
-        print("It's a pea")
-    else:
-        print("It's a cherry")
-else:
-    if green:
-        print("It's a watermelon")
-    else:
-        print("It's a pumpkin")
+song = song.replace(' m', ' M')
+
+print(song)
+
+# Ch5 practice # 2
+print()
+
+questions = ["We don't serve strings around here. Are you a string?",
+             "What is said on Father's Day in the forest?",
+             "What makes the sound 'Sis! Boom! Bah!'?"
+             ]
+answers = ["An exploding sheep.",
+           "No, I'm a frayed knot.",
+           "'Pop!' goes the weasel."
+           ]
+
+for k in range(3):
+    print(f'Q : {questions[k]}\nA : {answers[k]}')
+
+# Ch5 practice # 3
+print()
+
+a = 'roast beef'
+b = 'ham'
+c = 'head'
+d = 'clam'
+
+print("My kitty cat likes %s, My kitty cat likes %s, My kitty cat fell on his %s \
+And now thinks he's a %s." % (a, b, c, d))
