f33ec6c230d1f61471f459930625098461b4ef98
Jeyoon <rj1031@naver.com>
2023-01-23 22:34:59 +0900

    A2Z_Ch6_01

--- 2023_01_23.py
+++ 2023_01_23.py
@@ -1,10 +1,21 @@
 # A to Z reminding
 
-# Ch5 practice # 6
+# numbers = [2, 3, 5]
+# position = 0
+# while position < len(numbers):
+#     number = numbers[position]
+#     if number % 2 == 0:
+#         print('Found even number', number)
+#         break
+#     position += 1
+# else:
+#     print('No even number found')
 
-inp = input("input your name : ")
-inp = inp.capitalize()
+# Ch6 practice # 1
 
-print("%sy Mc%sface" % (inp, inp))
-print("{}y Mc{}face".format(inp, inp))
-print(f'{inp}y Mc{inp}face')
+numbers = list()
+for number in range(0, 4):
+    numbers.append(-number + 3)
+print(numbers)
+
+
