7bd435b22588f44c0b7b24b5504d083fccb72381
Jeyoon <rj1031@naver.com>
2023-01-23 21:48:50 +0900

    A2Z_Ch5_03

--- 2023_01_23.py
+++ 2023_01_23.py
@@ -1,30 +1,10 @@
 # A to Z reminding
 
-# Ch5 practice # 4
+# Ch5 practice # 6
 
-salutation = 'Mr.'
-name = 'Kim'
-product = 'pen'
-verbed = 'broken'
-room = 'room'
-animals = 'dogs'
-amount = 'account'
-percent = '100'
-spokesman = 'Jason'
-job_title = 'CEO'
+inp = input("input your name : ")
+inp = inp.capitalize()
 
-letter = '''Dear {} {},
-    Thank you for your letter. We are sorry that our {} \
-{} in your {}. Please note that it should never \
-be used in a {}, especially near any {}.
-
-    Send us your receipt and {} for shipping and handling. \
-We will send you another {} that, in our tests, \
-is {}% less likely to have {}.
-
-    Thank you for your support.
-    Sincerely,
-    {}
-    {}'''.format(salutation, name, product, verbed, room, room, animals, amount, product, percent, verbed, spokesman, job_title)
-
-print(letter)
+print("%sy Mc%sface" % (inp, inp))
+print("{}y Mc{}face".format(inp, inp))
+print(f'{inp}y Mc{inp}face')
