25c39f393b00641340ed46ab0c1d408bc5db47cf
Jeyoon <rj1031@naver.com>
2023-01-24 11:49:51 +0900

    A2Z_Ch8_01~05

--- 2023_01_23.py
+++ 2023_01_23.py
@@ -1,36 +1,14 @@
 # A to Z reminding
 
-# Ch7 practice # 10
+# Ch8 practice # 1 ~ # 5
 
-even_numbs = [num for num in range(10) if num % 2 == 0]
-print(even_numbs)
+e2f = {'dog': 'chien', 'cat': 'chat', 'walrus': 'morse'}
 
-# Ch7 practice # 11
+print(e2f['walrus'])
 
-# rhymes의 각 튜플 첫 번째, 두 번째에서,
-# start1의 첫 글자를 대문자로 만들고 각 단어 뒤에 느낌표와 공백 출력
-# 이어서 rhymes의 첫 번째 문자열 대문자, 느낌표 출력
-# 두 번째는 start2와 공백 출력 후 두 번째 문자열과 마침표를 출력
-# ex) Fee! Fie! Foe! Flop!
-#     Someone better get a mop.
-#     ...
+f2e = {values: keys for keys, values in e2f.items()}
+print(f2e)
 
-start1 = ["fee", "fie", "foe"]
-rhymes = [("flop", "get a mop"),
-          ("fope", "turn the rope"),
-          ("fa", "get your ma"),
-          ("fudge", "call the judge"),
-          ("fat", "pet the cat"),
-          ("fog", "walk the dog"),
-          ("fun", "say we're done"),
-          ]
-start2 = "Someone better"
+print(f2e['chien'])
 
-for idx in range(0, len(start1)):
-    start1[idx] = start1[idx].capitalize()
-
-for k, j in rhymes:
-    for i in range(0, len(start1)):
-        print(f"{start1[i].capitalize()}!", end=' ')
-    print(f'{k.capitalize()}!')
-    print(f'{start2} {j}.')
+print(list(e2f.keys()))
