865cc4c80e9ee85641839f4a038f75845cf11da9
Jeyoon <rj1031@naver.com>
2023-01-24 20:23:26 +0900

    A2Z_Ch9_#2

--- A2Z.py
+++ A2Z.py
@@ -1,6 +1,15 @@
 # A to Z reminding
 
-# Ch9 practice # 1
+# Ch9 practice # 2
 
-def good():
-    return ['Harry', 'Ron', 'Hermione']
+def get_odds():
+    for odd in range(10):
+        if odd % 2 == 1:
+            yield odd
+
+
+count = 0
+for x in get_odds():
+    count += 1
+    if count == 3:
+        print(x)
