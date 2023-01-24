94420f413afd13b93b9347c408ccc1abf9f25e2a
Jeyoon <rj1031@naver.com>
2023-01-24 20:48:18 +0900

    A2Z_Ch9_#4

--- A2Z.py
+++ A2Z.py
@@ -1,23 +1,18 @@
 # A to Z reminding
 
-# Ch9 practice # 3
+# Ch9 practice # 4
+class OopsException(Exception):
+    pass
 
-def test(func):
-    def new_func(*args, **kwargs):
-        print('start')
-        result = func(*args, **kwargs)
-        print(result)
-        print('end')
-        return result
-
-    return new_func
-
-# @test >> 데커레이터 수동 할당 대안
-def subs_ints(a, b):
-    return a - b
-
-
-new_subs_ints = test(subs_ints)  # 데커레이터 수동 할당
-
-print(subs_ints(5, 3))
-new_subs_ints(5, 3)  # 수동 할당 결과
+while True:
+    try:
+        things = input('input only q : ')
+        if things == 'q':
+            raise OopsException('Well done! It\'s q!')
+        print('You put wrong thing..')
+    except OopsException as err:
+        print(err)
+    else:
+        print("It's okay. Try again.")
+    finally:
+        print('Keep going!')
