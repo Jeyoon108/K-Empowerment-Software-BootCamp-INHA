75d187536333e37fcf311ea358b6479a533fe225
Jeyoon <rj1031@naver.com>
2023-01-24 20:31:33 +0900

    A2Z_Ch9_#3

--- A2Z.py
+++ A2Z.py
@@ -1,15 +1,23 @@
 # A to Z reminding
 
-# Ch9 practice # 2
+# Ch9 practice # 3
 
-def get_odds():
-    for odd in range(10):
-        if odd % 2 == 1:
-            yield odd
+def test(func):
+    def new_func(*args, **kwargs):
+        print('start')
+        result = func(*args, **kwargs)
+        print(result)
+        print('end')
+        return result
+
+    return new_func
+
+# @test >> 데커레이터 수동 할당 대안
+def subs_ints(a, b):
+    return a - b
 
 
-count = 0
-for x in get_odds():
-    count += 1
-    if count == 3:
-        print(x)
+new_subs_ints = test(subs_ints)  # 데커레이터 수동 할당
+
+print(subs_ints(5, 3))
+new_subs_ints(5, 3)  # 수동 할당 결과
