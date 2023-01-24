a81f03112add4de675d7d44c3c9a74d7fec966c4
Jeyoon <rj1031@naver.com>
2023-01-25 00:20:37 +0900

    A2Z_Ch10_class

--- A2Z.py
+++ A2Z.py
@@ -1,18 +1,20 @@
 # A to Z reminding
 
-# Ch9 practice # 4
-class OopsException(Exception):
-    pass
+# Ch10 practice class
 
-while True:
-    try:
-        things = input('input only q : ')
-        if things == 'q':
-            raise OopsException('Well done! It\'s q!')
-        print('You put wrong thing..')
-    except OopsException as err:
-        print(err)
-    else:
-        print("It's okay. Try again.")
-    finally:
-        print('Keep going!')
+class Maple:
+    def __init__(self, name, job, skills):
+        self.name = name
+        self.job = job
+        self.skills = skills.split('/')
+        print(f'"system" {self.name}이(가) 생성되었습니다.')
+
+    def status(self):
+        print(f'당신의 캐릭터 이름은 {self.name}, 직업은 {self.job}입니다.')
+        print(f'보유중인 스킬 : {self.skills}')
+
+m1 = Maple('킹왕짱', '아델', '디바이드/블로섬/게더링/다이크')
+m1.status()
+print()
+m2 = Maple('널뛰기공주', '에반', '서클 오브 마나IV/드래곤 브레스/서클 오브 어스/서클 오브 썬더')
+m2.status()
